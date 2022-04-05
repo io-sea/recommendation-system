4361c8f500dfc2c31eccc5da6f10c28ea3066a3b
Salim Mimouni <salim.mimouni@atos.net>
2022-04-05 18:00:19 +0200

    scheduling linear app

--- cluster_simulator/application.py
+++ cluster_simulator/application.py
@@ -15,6 +15,16 @@ def convert_size(size_bytes):
     return "%s %s" % (s, size_name[i])
 
 
+def get_tier(tier, cluster):
+    if isinstance(tier, int):
+        tier = cluster.tiers[tier]
+    if isinstance(tier, str):
+        for cluster_tier in cluster.tiers:
+            if tier == cluster_tier.name:
+                tier = cluster_tier
+    return tier
+
+
 class Cluster:
     def __init__(self, env, compute_nodes=1, cores_per_node=2, tiers=[]):
         self.compute_nodes = simpy.Resource(env, capacity=compute_nodes)
@@ -80,7 +90,7 @@ class IO_Compute:
         self.duration = duration
         self.cores = cores
 
-    def play(self, cluster, env):
+    def run(self, env, cluster):
         used_cores = []
         for i in range(self.cores):
             core = cluster.compute_cores.request()
@@ -95,19 +105,15 @@ class IO_Compute:
 
 
 class IO_Phase:
-    def __init__(self, volume, pattern=1):
+    def __init__(self, operation='read', volume=1e9, pattern=1):
         """
         pattern = 0.8:
             80% sequential and 20% random.
             blocksize for moment is not variable."""
+        self.operation = operation
+        assert self.operation in ['read', 'write']
         self.volume = volume
         self.pattern = pattern
-
-
-class Read_IO_Phase(IO_Phase):
-    def __init__(self, volume, pattern):
-        super().__init__(volume, pattern)
-        self.operation = 'read'
         logger.info(self.__str__())
 
     def __str__(self):
@@ -115,22 +121,10 @@ class Read_IO_Phase(IO_Phase):
         description = "-------------------\n"
         description += (f"{self.operation.upper()} I/O Phase of volume {convert_size(self.volume)} with pattern: {io_pattern}\n")
         return description
-        #self.read_bandwidth = tier.bandwidth['read']
 
-    @staticmethod
-    def get_tier(tier, cluster):
-        if isinstance(tier, int):
-            tier = cluster.tiers[tier]
-        if isinstance(tier, str):
-            for cluster_tier in cluster.tiers:
-                if tier == cluster_tier.name:
-                    tier = cluster_tier
-        return tier
-
-    def schedule(self, env, cluster, tier=None, cores=1):
+    def run(self, env, cluster, cores=1, tier=None):
         # Pre compute parameters
-        tier = self.get_tier(tier, cluster)
-
+        tier = get_tier(tier, cluster)
         bandwidth = tier.bandwidth[self.operation]['seq'] * self.pattern + tier.bandwidth[self.operation]['rand']*(1-self.pattern) * compute_share_model(cores)
 
         used_cores = []
@@ -138,7 +132,7 @@ class Read_IO_Phase(IO_Phase):
             core = cluster.compute_cores.request()
             used_cores.append(core)
             yield core
-        logger.info(f"Start Read I/O Phase with volume={convert_size(self.volume)} at {env.now}")
+        logger.info(f"Start Read I/O Phase with volume = {convert_size(self.volume)} at {env.now}")
         logger.info(f"Reading I/O with bandwidth = {bandwidth} MB/s")
         yield env.timeout((self.volume/1e6)/bandwidth)
         for core in used_cores:
@@ -158,14 +152,15 @@ class Read_IO_Phase(IO_Phase):
 
 
 class Application:
-    def __init__(self, env, store, compute=[0, 10], read=[1e9, 0], write=[0, 5e9]):
+    def __init__(self, env, store, compute=[0, 10], read=[1e9, 0], write=[0, 5e9], tiers=[0, 0]):
         self.env = env
         self.store = store
         self.compute = compute
         self.read = read
         self.write = write
+        self.tiers = tiers
         # ensure format is valid, all list are length equal
-        assert all([len(lst) == len(self.compute) for lst in [self.read, self.write]])
+        assert all([len(lst) == len(self.compute) for lst in [self.read, self.write, self.tiers]])
         # schedule all events
         self.schedule()
 
@@ -175,40 +170,72 @@ class Application:
         io_compute = IO_Compute(duration, cores)
         self.store.put(io_compute)
 
-    def put_io(self, volume):
+    def put_io(self, operation, volume, pattern=1):
         # self.env.process(run_io_phase(cluster, self.env, volume))
         # store.put(run_io_phase(cluster, self.env, volume))
-        io_phase = IO_Phase(volume)
+        io_phase = IO_Phase(operation=operation, volume=volume, pattern=pattern)
         self.store.put(io_phase)
 
     def schedule(self):
         for i in range(len(self.compute)):
             # read is prioritary
             if self.read[i] > 0:
-                self.put_io(volume=self.read[i])
+                read_io = IO_Phase(operation='read', volume=self.read[i])
+                self.store.put(read_io)
+                #self.put_io(operation='read', volume=self.read[i])
             # then write
             if self.write[i] > 0:
-                self.put_io(volume=self.write[i])
+                write_io = IO_Phase(operation='write', volume=self.write[i])
+                self.store.put(write_io)
             # then compute duration = diff between two events
             if i < len(self.compute) - 1:
                 duration = self.compute[i+1] - self.compute[i]
                 self.put_compute(duration)
 
     def run(self, cluster):
-        while True:
-            item = yield store.get()
-            yield self.env.process(item.play(cluster, self.env))
+        item_number = 0
+        while self.store.items is not None:
+            item = yield self.store.get()
+            if isinstance(item, IO_Compute):
+                # compute phase
+                yield self.env.process(item.run(self.env, cluster))
+            else:
+                # io phase
+                print(f"{item_number} from a list of {len(cluster.tiers)}")
+                tier = cluster.tiers[self.tiers[item_number]]
+
+                #assert isinstance(tier, Tier)
+                yield self.env.process(item.run(self.env, cluster, cores=1, tier=tier))
+                item_number += 1
 
     # def run(self, env, cluster):
 if __name__ == '__main__':
     env = simpy.Environment()
-    cluster = Cluster(env)
+
     store = simpy.Store(env, capacity=1000)
     # env.process(run_compute_phase(cluster, env, duration=10, cores=3))
-    app = Application(env, store)
+    compute = [0, 10]
+    read = [1e9, 0]
+    write = [0, 5e9]
+    tiers = [0, 1]
+    nvram_bandwidth = {'read':  {'seq': 780, 'rand': 760},
+                       'write': {'seq': 515, 'rand': 505}}
+    ssd_bandwidth = {'read':  {'seq': 210, 'rand': 190},
+                     'write': {'seq': 100, 'rand': 100}}
+
+    ssd_tier = Tier(env, 'SSD', bandwidth=ssd_bandwidth, capacity=200e9)
+    nvram_tier = Tier(env, 'NVRAM', bandwidth=nvram_bandwidth, capacity=80e9)
+    cluster = Cluster(env, tiers=[ssd_tier, nvram_tier])
+    app = Application(env, store,
+                      compute=compute,
+                      read=read,
+                      write=write,
+                      tiers=tiers)
+    env.process(app.run(cluster))
+    app.env.run()
     # app.put_compute(duration=10, cores=2)
     # app.put_io(volume=2e9)
     # job.put_compute(duration=10, cores=2)
     # env.process(run_io_phase(cluster, env, 10e9))
-    env.process(app.run(cluster))
-    env.run(until=20)
+    # env.process(app.run(cluster))
+    # env.run(until=20)
--- cluster_simulator/tests/test_cluster_simulator.py
+++ cluster_simulator/tests/test_cluster_simulator.py
@@ -4,7 +4,7 @@ import numpy as np
 import simpy
 
 from cluster_simulator.application import Cluster, Application, Tier
-from cluster_simulator.application import Read_IO_Phase
+from cluster_simulator.application import IO_Phase, IO_Compute
 
 
 class TestCluster(unittest.TestCase):
@@ -28,25 +28,56 @@ class TestCluster(unittest.TestCase):
         print(cluster)
         self.assertIsInstance(cluster.compute_nodes, simpy.Resource)
         self.assertIsInstance(cluster.compute_cores, simpy.Resource)
-        #self.assertIsInstance(cluster.storage_capacity, simpy.Container)
-        #self.assertIsInstance(cluster.storage_speed, simpy.Container)
+
+    def test_compute_phase(self):
+        cluster = Cluster(self.env, compute_nodes=3, cores_per_node=4)
+        compute_phase = IO_Compute(duration=10)
+        self.env.process(compute_phase.run(self.env, cluster))
+        self.env.run()
 
     def test_read_io_phase(self):
         cluster = Cluster(self.env, tiers=[self.ssd_tier, self.nvram_tier])
-        read_io = Read_IO_Phase(volume=9e9, pattern=0.2)
-        # print(read_io)
-        self.env.process(read_io.schedule(self.env, cluster, 1))
+        #read_io = Read_IO_Phase(volume=9e9, pattern=0.2)
+        read_io = IO_Phase(operation='read', volume=9e9, pattern=0.2)
+        self.env.process(read_io.run(self.env, cluster, tier=1))
         self.env.run()
 
-    def test_application_basic(self):
+    def test_write_io_phase(self):
+        cluster = Cluster(self.env, tiers=[self.ssd_tier, self.nvram_tier])
+        #read_io = Read_IO_Phase(volume=9e9, pattern=0.2)
+        write_io = IO_Phase(operation='write', volume=9e9, pattern=0.2)
+        self.env.process(write_io.run(self.env, cluster, tier=1))
+        self.env.run()
+
+    def test_application_init(self):
         # Simple app: read 1GB -> compute 10s -> write 5GB
         compute = [0, 10]
         read = [1e9, 0]
         write = [0, 5e9]
+        tiers = [0, 1]
         app = Application(self.env, self.store,
                           compute=compute,
                           read=read,
-                          write=write)
-        print(app.store.capacity)
-        print(app.store.items)
+                          write=write,
+                          tiers=tiers)
+        # print(app.store.capacity)
+        # print(app.store.items)
         self.assertEqual(len(app.store.items), 3)
+
+    def test_application_run(self):
+        cluster = Cluster(self.env, tiers=[self.ssd_tier, self.nvram_tier])
+        # Simple app: read 1GB -> compute 10s -> write 5GB
+        compute = [0, 10]
+        read = [1e9, 0]
+        write = [0, 5e9]
+        tiers = [0, 1]
+        app = Application(self.env, self.store,
+                          compute=compute,
+                          read=read,
+                          write=write,
+                          tiers=tiers)
+
+        self.env.process(app.run(cluster))
+        self.env.run()
+        # self.env.run(until=25)
+        #self.assertEqual(len(app.store.items), 3)
