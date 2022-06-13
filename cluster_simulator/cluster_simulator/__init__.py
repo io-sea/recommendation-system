b49e53051a9b259e590dcd8510fceb7f9a13d868
Salim Mimouni <salim.mimouni@atos.net>
2022-06-13 17:24:20 +0200

    bds email

--- cluster_simulator/cluster_simulator/__init__.py
+++ cluster_simulator/cluster_simulator/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Cluster Simulator."""
 
 __author__ = """Bull/ATOS"""
-__email__ = 'bds_datamanagement@atos.net'
+__email__ = 'bds-datamanagement@atos.net'
 __version__ = '0.1.0'
