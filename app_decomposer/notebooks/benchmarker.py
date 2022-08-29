4f02837174561a1e7a1815a0e5d2b2e02adbf7bc
Salim Mimouni <salim.mimouni@atos.net>
2022-08-29 10:43:00 +0200

    cleaning repo

---
+++ app_decomposer/notebooks/benchmarker.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import ruptures as rpt
+import os, random, time
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# choose a job
+def list_jobs(dataset_path = "/home_nfs/mimounis/iosea-wp3-recommandation-system/dataset_generation/dataset_generation"):
+    # /home_nfs/mimounis/iosea-wp3-recommandation-system/dataset_generation/dataset_generation
+    job_files = []
+    job_ids = []
+    dataset = []
+    for root, dirs, files in os.walk(dataset_path):
+        for csv_file in files:
+            if csv_file.endswith(".csv"):
+                job_files.append(os.path.join(root, csv_file))
+                job_ids.append(csv_file.split("_")[-1].split(".csv")[0])
+                dataset.append(os.path.split(root)[-1])
+    return job_files, job_ids, dataset
+    
+if __name__ == '__main__':
+    
+    job_files, job_ids, datasets = list_jobs()
+
+
+    df = pd.DataFrame(columns=["dataset", "jobid", "timeserie", "signal_length", "n_brkpts",
+                            "algo_name", "cost_function", "loss", "penalty", "computation_time"])
+    
+    
+
+    algos_names = ["Pelt", "Binseg", "Botup", "Wind"]
+    
+    output_file = os.path.join("/home_nfs/mimounis/iosea-wp3-recommandation-system/job_decomposer/job_decomposer", "_".join(algos_names) + "_bench_2.csv")
+    
+    
+    for job_file, job_id, dataset in zip(job_files, job_ids, datasets):
+        df_signal = pd.read_csv(job_file, index_col=0)
+        for ts in ["bytesWritten", "bytesRead"]:
+            signal = df_signal[[ts]].to_numpy()
+            signal_dim = signal.shape[1]
+
+            # define costs
+            costs = [rpt.costs.CostL1(), rpt.costs.CostL2(), rpt.costs.CostNormal(), rpt.costs.CostLinear(),
+                    rpt.costs.CostCLinear(), rpt.costs.CostAR(order=3), rpt.costs.CostMl(metric=np.eye(signal_dim)), rpt.costs.CostRank()]
+            costs_names = ["L1", "L2", "Gauss", "Linear", "Clinear", "AR", "Mala", "Rank"]
+
+            # algos = [rpt.Pelt(custom_cost=cost, min_size=2), rpt.Binseg(custom_cost=cost),
+            #          rpt.BottomUp(custom_cost=cost), rpt.Window(custom_cost=cost, width=10)]
+
+            algos = [rpt.Pelt, rpt.Binseg, rpt.BottomUp, rpt.Window]
+
+            algos_names = ["Pelt", "Binseg", "Botup", "Wind"]
+
+            penalties = np.logspace(-3, 3, num=6).tolist()
+    
+            for algo, algo_name in zip(algos, algos_names):
+                for cost, costs_name in zip(costs[0:3], costs_names):
+                    for pen in penalties:
+                        print(f"{job_id=} | {algo_name=} | {costs_name=} | {pen=}")
+                        try:
+                            start_time = time.time()
+                            result = algo(custom_cost=cost, min_size=2).fit_predict(signal, pen)
+                            duration = time.time() - start_time
+                            n_brkpts = len(result) - 1
+                            loss = cost.sum_of_costs(result)
+                            df = df.append({"dataset": dataset,
+                                            "jobid": job_id,
+                                            "timeserie": ts,
+                                            "signal_length": signal.shape[0],
+                                            "n_brkpts": n_brkpts,
+                                            "algo_name": algo_name,
+                                            "cost_function": costs_name,
+                                            "loss": loss,
+                                            "penalty": pen,
+                                            "computation_time": duration,
+                                            },
+                                        ignore_index=True)
+                            
+                        except:
+                            pass
+                        
+            df.to_csv(output_file)
+                        
+    
+    #df.to_csv("_".join(algos_names) + "_bench.csv")
+    
+        
+        
---
+++ app_decomposer/notebooks/utils.py
@@ -0,0 +1,293 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import ruptures as rpt
+import pwlf
+import numpy as np
+from scipy import integrate
+import os, random
+from kneed import KneeLocator
+import numpy as np
+import time
+from sklearn.cluster import MiniBatchKMeans, KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+import random
+
+def choose_random_job(dataset_path = "C:\\Users\\a770398\\IO-SEA\\io-sea-3.4-analytics\\dataset_generation\\dataset_generation"):
+    job_files = []
+    for root, dirs, files in os.walk(dataset_path):
+        for csv_file in files:
+            if csv_file.endswith(".csv"):
+                job_files.append(os.path.join(root, csv_file))
+    return random.choice(job_files)
+
+
+def list_jobs(dataset_path = "C:\\Users\\a770398\\IO-SEA\\io-sea-3.4-analytics\\dataset_generation\\dataset_generation"):
+    job_files = []
+    job_ids = []
+    dataset = []
+    for root, dirs, files in os.walk(dataset_path):
+        for csv_file in files:
+            if csv_file.endswith(".csv"):
+                job_files.append(os.path.join(root, csv_file))
+                job_ids.append(csv_file.split("_")[-1].split(".csv")[0])
+                dataset.append(os.path.split(root)[-1])
+    return job_files, job_ids, dataset
+
+
+
+def plot_job_phases(x, signal, breakpoints):
+    
+    x = ((np.array(x) - x[0])/5).tolist()
+    plt.rcParams["figure.figsize"] = (12, 5)
+    plt.rcParams['figure.facecolor'] = 'gray'
+    
+    plt.plot(x, signal, lw=2, label="IO")
+    for i_brk, brk in enumerate(breakpoints[:-1]): #avoid last point
+        if i_brk % 2 == 0: # opening point
+            plt.plot(x[brk], signal[brk], '>g')
+        else:
+            plt.plot(x[brk], signal[brk], '<r')
+    plt.grid(True)
+    plt.show()
+    
+def plot_job_phases2(x, signal, breakpoints):
+    signal = np.array(signal)
+    #x = ((np.array(x) - x[0])/5).tolist()
+    fig, ax = rpt.display(signal, breakpoints)
+    #ax[0].plot(x, signal, lw=2, label="IO", color='k')
+    
+    for i_brk, brk in enumerate(breakpoints[:-1]): #avoid last point
+        if i_brk % 2 == 0: # opening point
+            ax[0].plot([brk], signal[brk], '>g')
+        else:
+            ax[0].plot([brk], signal[brk], '<r')
+    plt.grid(True)
+    plt.show()
+    
+    
+def get_df_by_job_id(job_id=1391):
+    job_files, job_ids, datasets = list_jobs()
+    csv_file = job_files[job_ids.index(str(job_id))]
+    print(f"retrieving {csv_file}")
+    return pd.read_csv(csv_file, index_col=0)
+
+
+def plot_bench_decomposition(bench_file="Pelt_Binseg_Botup_Wind_bench.csv", idx=0):
+        
+
+    bench_df = pd.read_csv(bench_file, index_col=0)
+    if idx == "rnd":
+        idx = int(bench_df.sample().index.values[0])
+
+    algos = [rpt.Pelt, rpt.Binseg, rpt.BottomUp, rpt.Window]
+    algos_names = ["Pelt", "Binseg", "Botup", "Wind"]
+    # define costs
+    costs = [rpt.costs.CostL1(), rpt.costs.CostL2(), rpt.costs.CostNormal(), rpt.costs.CostLinear(),
+            rpt.costs.CostCLinear(), rpt.costs.CostAR(order=3), rpt.costs.CostMl(metric=np.eye(1)), rpt.costs.CostRank()]
+    costs_names = ["L1", "L2", "Gauss", "Linear", "Clinear", "AR", "Mala", "Rank"]
+
+    #df.iloc[idx]["jobid"]
+    algo = algos[algos_names.index(bench_df.iloc[idx]["algo_name"])]
+    cost = costs[costs_names.index(bench_df.iloc[idx]["cost_function"])]
+    penalty = bench_df.iloc[idx]["penalty"]
+    timeserie = bench_df.iloc[idx]["timeserie"]
+
+    job_files, job_ids, datasets = list_jobs()
+    str_jobid = str(bench_df.iloc[idx]["jobid"])
+    print(f"{algo = }")
+    print(f"{cost = }")
+    csv_file = job_files[job_ids.index(str_jobid)]
+    print(f"{csv_file = }")
+    df = pd.read_csv(csv_file, index_col=0)
+
+    signal = df[[timeserie]].to_numpy().tolist()
+    x = df[["timestamp"]].to_numpy().tolist()
+    print(f"length of signal = {len(signal)}")
+    breakpoints = algo(custom_cost=cost, min_size=1, jump=1).fit_predict(df[[timeserie]].to_numpy(), pen=penalty)
+    loss = cost.sum_of_costs(breakpoints)
+    print(f"{penalty = }")
+    print(f"{loss = }")
+    print(f"number of breakpoints = {len(breakpoints)-1}")
+        
+    plot_job_phases2(x, signal, breakpoints)
+    
+
+def plot_bench_decomposition_cpd(bench_file="kernel_cpd.csv", idx = 0):
+    bench_df = pd.read_csv(bench_file, index_col=0)
+    
+    if idx == "rnd":
+        idx = int(bench_df.sample().index.values[0])
+
+    algo = rpt.KernelCPD
+    # define costs
+    kernels = ["rbf", "linear"]
+
+    kernel = bench_df.iloc[idx]["kernel"]
+    n_brkpts = bench_df.iloc[idx]["n_brkpts"]
+
+
+    job_files, job_ids, datasets = list_jobs()
+    str_jobid = str(bench_df.iloc[idx]["jobid"])
+    csv_file = job_files[job_ids.index(str_jobid)]
+    print(f"{csv_file = }")
+    df = pd.read_csv(csv_file, index_col=0)
+
+    timeserie = bench_df.iloc[idx]["timeserie"]
+    signal = df[[timeserie]].to_numpy().tolist()
+    x = df[["timestamp"]].to_numpy().tolist()
+    print(f"length of signal = {len(signal)}")
+    breakpoints = algo(kernel=kernel, min_size=1).fit_predict(df[[timeserie]].to_numpy(),
+                                                            n_bkps=n_brkpts)
+    print(f"{breakpoints = }")
+    print(f"number of breakpoints = {len(breakpoints)-1}")
+        
+    plot_job_phases2(x, signal, breakpoints)
+    
+
+def get_sum_of_cost(algo, n_bkps) -> float:
+    """Return the sum of costs for the change points `bkps`"""
+    bkps = algo.predict(n_bkps=n_bkps)
+    return algo.cost.sum_of_costs(bkps)
+
+def get_optimal_breakpoints(signal, algo=rpt.KernelCPD, kernel="linear"):
+    initialized_algo = algo(kernel=kernel, min_size=1).fit(signal)
+    n_bkps_max = int((np.sqrt(len(signal))))
+    # segment
+    _ = initialized_algo.predict(n_bkps_max)
+    array_of_n_bkps = np.arange(1, n_bkps_max + 1)
+    costs = [get_sum_of_cost(algo=initialized_algo, n_bkps=n_bkps) for n_bkps in array_of_n_bkps]
+    elbow = KneeLocator(array_of_n_bkps, costs, S= 1,
+                         curve="convex",  direction="decreasing").elbow
+    optimal_n_breakpoints = elbow if elbow else n_bkps_max
+    optimal_breakpoints = initialized_algo.predict(n_bkps=optimal_n_breakpoints)
+    optimal_cost = costs[array_of_n_bkps.tolist().index(optimal_n_breakpoints)]
+    
+    return optimal_n_breakpoints, optimal_breakpoints, optimal_cost
+
+
+def get_kmeans_breakpoints(signal, v0_threshold = 0.05, merge=False):
+    """_summary_
+
+    Args:
+        signal (ndarray): _description_
+    """
+    v0_weight = 1
+    n_clusters = 2
+    while v0_weight > v0_threshold:
+        kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(signal)
+        v0_indexes = np.where(kmeans.labels_==0, True, False)
+        v0_weight = signal[v0_indexes].sum() / signal.sum()
+        n_clusters += 1
+        if n_clusters >= len(signal):
+            # if n_clusters equals signal points, exit the loop
+            break
+        
+    ab = np.arange(len(signal))
+    print(f"{n_clusters = }")
+    print(f"{v0_weight = }")
+    # changes=x[np.insert(np.where(np.diff(kmeans.labels_)!=0, True, False), 0, False)]
+    # merge v1+ clusters together
+    if merge:
+        labels = np.where(kmeans.labels_ > 0, 1, 0)
+    else:
+        labels = kmeans.labels_
+    print(f"labels = {labels}")
+    return  ab[np.insert(np.where(np.diff(labels)!=0, True, False), 0, False)].tolist(), labels
+
+
+
+def clustering(signal, n):
+    k_means = KMeans(init='k-means++', n_clusters=n, n_init=10)
+    t0 = time.time()
+    k_means.fit(signal)
+    t_batch = time.time() - t0
+    k_means_labels = k_means.labels_
+    k_means_cluster_centers = k_means.cluster_centers_
+    #print(k_means_labels, t_batch)
+    return k_means_labels
+
+def find_best_cluster(signal, min_diff=0.02, max_n=50):
+    step = 2
+    n_clusters = step
+    n_best = step
+    best_score = -1
+    n_samples = len(signal)
+    best_preds = np.random.choice(n_clusters, n_samples)  # init with random labels
+
+    while step <= max_n and n_clusters < n_samples:
+        preds = clustering(signal, step)
+        score = silhouette_score(signal, preds, metric="euclidean")
+        #print(score)
+        if score > best_score * (1 + min_diff):  # if better score
+            best_score = score
+            best_preds = preds
+            n_best = step
+        step += 1
+        n_clusters += 1
+    
+    return best_preds, n_best
+
+def changepoint_detection(changes):
+    result=[]
+    
+    for idx in range(len(changes)-1):
+        if changes[idx] != changes[idx+1]:
+            #print(idx)
+            result.append(idx+1)
+    return result
+
+
+def get_kmeans_original_breakpoints(signal):
+    k_means_labels, n = find_best_cluster(signal)
+    
+    print(f"n_clusters = {n}")
+    print(f"labels = {k_means_labels}")
+    result = changepoint_detection(k_means_labels)
+    return result, k_means_labels
+
+
+def reconstruct_pwlf(signal, breakpoints):
+    ab = np.arange(len(signal))
+    my_pwlf = pwlf.PiecewiseLinFit(ab, signal, degree=0)
+    my_pwlf.fit_with_breaks(breakpoints)
+    return my_pwlf.predict(ab)
+    
+
+def show_breakpoints(signal, breakpoints, breakpoints2=[], labels=[], labels2=[]):
+    signal = signal.flatten().tolist()
+    ab = np.arange(len(signal))
+    #p_signal = reconstruct_pwlf(signal, breakpoints)
+
+    #plt.plot(ab, p_signal, linestyle='dashed', linewidth=0.5)
+    for i, bpt in enumerate(breakpoints):
+        plt.axvline(x=bpt, linestyle='dashed',  color="black", alpha=0.5)
+        if labels:
+            plt.annotate(labels[bpt], (ab[bpt], signal[bpt]), color="black")
+            
+                
+    if breakpoints2:
+        for bpt in breakpoints2:
+            plt.axvline(x=bpt, linestyle='dotted', color="red", alpha=0.7)
+            if labels2:
+                plt.annotate(labels2[bpt], (ab[bpt], signal[bpt]*1.3), color="red")
+        
+    plt.plot(ab, signal, label="original signal")
+    plt.show()
+    
+    
+    
+def show_reconstruction(signal, p_signal):
+    ab = np.arange(len(signal))
+    plt.figure(0)
+    plt.plot(ab, signal, label="original signal")
+    plt.plot(ab, p_signal, label="decomposed signal", linewidth=0.7, linestyle="dashed", color="black")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    plt.figure(1)
+    plt.plot(ab, np.cumsum(signal), label="original signal")
+    plt.plot(ab, np.cumsum(p_signal), label="decomposed signal",
+            linewidth=0.7, linestyle="dashed", color="black")
+    plt.grid(True)
+    plt.show()
