4d63ced1e623ed35f853c56e7db3c0419250ff4a
Salim Mimouni <salim.mimouni@atos.net>
2022-11-04 17:32:32 +0100

    adding connection to real jobs on kiwi to process other timeseries

--- app_decomposer/app_decomposer/__init__.py
+++ app_decomposer/app_decomposer/__init__.py
@@ -7,3 +7,38 @@ DEFAULT_CONFIGURATION = os.path.join(CURRENT_DIR, "defaults", "config.yaml")
 __author__ = """Salim Mimouni"""
 __email__ = 'salim.mimouni@atos.net'
 __version__ = '0.1.0'
+
+
+"""Init for datafetchers.
+Sets the default configuration as global variables.
+"""
+
+# The API_DICT_TS structure represents how the time-series are reachable through the API. Each
+# time-series is accessed by its group (keys in the dictionary API_DICT_TS).
+# This is the exhaustive reference list of available time-series in the database.
+# Note, in the future, this hardcoded structure should be dynamically retrieved from the API using
+# a dedicated method (to be designed).
+API_DICT_TS = {
+    'volume': ['bytesRead', 'bytesWritten'],
+    'processCount': ['ioActiveProcessCount', 'processCount'],
+    'ioEfficiencyDistribution': ['accelerated', 'accelPossible', 'accelImpossible'],
+    'accessPattern': ['accessRandRead', 'accessSeqRead', 'accessStrRead', 'accessUnclRead',
+                      'accessRandWrite', 'accessSeqWrite', 'accessStrWrite', 'accessUnclWrite'],
+    'files': ['filesCreated', 'filesDeleted', 'filesRO', 'filesWO', 'filesRW'],
+    'ioSizes': ['IOSizeReadRange0', 'IOSizeReadRange1', 'IOSizeReadRange2', 'IOSizeReadRange3',
+                'IOSizeReadRange4', 'IOSizeReadRange5', 'IOSizeReadRange6', 'IOSizeWriteRange0',
+                'IOSizeWriteRange1', 'IOSizeWriteRange2', 'IOSizeWriteRange3', 'IOSizeWriteRange4',
+                'IOSizeWriteRange5', 'IOSizeWriteRange6'],
+    'ioDurations': ['IODurationReadRange0', 'IODurationReadRange1', 'IODurationReadRange2',
+                    'IODurationReadRange3', 'IODurationReadRange4', 'IODurationReadRange5',
+                    'IODurationReadRange6', 'IODurationReadRange7', 'IODurationReadRange8',
+                    'IODurationReadRange9', 'IODurationReadRange10', 'IODurationReadRange11',
+                    'IODurationReadRange12', 'IODurationReadRange13', 'IODurationReadRange14',
+                    'IODurationReadRange15', 'IODurationWriteRange0', 'IODurationWriteRange1',
+                    'IODurationWriteRange2', 'IODurationWriteRange3', 'IODurationWriteRange4',
+                    'IODurationWriteRange5', 'IODurationWriteRange6', 'IODurationWriteRange7',
+                    'IODurationWriteRange8', 'IODurationWriteRange9', 'IODurationWriteRange10',
+                    'IODurationWriteRange11', 'IODurationWriteRange12', 'IODurationWriteRange13',
+                    'IODurationWriteRange14', 'IODurationWriteRange15'],
+    'operationsCount': ['operationRead', 'operationWrite']
+    }
--- app_decomposer/tests_integration/test_token_connector.py
+++ app_decomposer/tests_integration/test_token_connector.py
@@ -19,17 +19,21 @@ import urllib3
 
 from app_decomposer.api_connector import request_delegator
 from app_decomposer.config_parser import Configuration
+from app_decomposer.api_connector import TimeSeries
 
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-TEST_CONFIG = os.path.join(CURRENT_DIR, "test_data", "test_config.yaml")
+TEST_CONFIG_DEFAULT = os.path.join(CURRENT_DIR, "test_data", "test_config.yaml")
 TEST_CONFIG_KO = os.path.join(CURRENT_DIR, "test_data", "test_config_kc_ko.yaml")
+KIWI_CONFIG = os.path.join(CURRENT_DIR, "test_data", "test_kiwi_config.yaml")
 
+TEST_CONFIG = KIWI_CONFIG
 class TestKeycloakToken(unittest.TestCase):
     """ Test KeycloakToken functionalities """
 
     def setUp(self):
-        """ Prepare the test suite """
+        """ Prepare the test suite with self.config.
+        Enter path=KIWI_CONFIG for scraping data from kiwi0 IOI slogin node."""
         self.config = Configuration(path=TEST_CONFIG)
         # To disable useless warnings with requests module on https without certificate
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -49,6 +53,28 @@ class TestKeycloakToken(unittest.TestCase):
         resp = rqst.json()
         self.assertIn('username', resp)
         self.assertEqual(resp['username'], "ioi-admin")
+
+    @unittest.skipUnless(TEST_CONFIG == KIWI_CONFIG, "Skip this test because kiwi0 is not up")
+    def test_on_kiwi0(self):
+        """test if unittest activates on condition"""
+        self.assertEqual(TEST_CONFIG, KIWI_CONFIG)
+
+    @unittest.skipUnless(TEST_CONFIG == KIWI_CONFIG, "Skip this test because kiwi0 is not up")
+    def test_get_ts_on_kiwi0(self):
+        """tests if can get timeseries from kiwi0"""
+        token = self.config.get_kc_token()
+        # To disable useless warnings with requests module on https without certificate
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+        api_uri = f"{self.config.get_api_uri()}:{self.config.get_api_port()}"
+        ts_data = dict()
+        for ts in ["volume", "operationsCount", "accessPattern"]:
+            ts_data[ts]  = TimeSeries(api_uri=api_uri,
+                                      api_token=f"Bearer {token}",
+                                      object_id='6352a0af7992958a4b807942',
+                                      type_series=ts).get_data_by_label()
+
+        print(ts_data)
 
 
 
