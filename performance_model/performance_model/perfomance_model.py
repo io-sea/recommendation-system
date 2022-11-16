bfab831f2a151269d816a255b9388e8ea3801b60
Trong Ton PHAM <trong-ton.pham@atos.net>
2022-11-16 17:06:35 +0100

    add test dataset + code refactoring

--- performance_model/performance_model/perfomance_model.py
+++ performance_model/performance_model/perfomance_model.py
@@ -8,27 +8,7 @@ import os
 import sys
 sys.path.append('../../')
 import pandas as pd
-from fakeapp_generator.fakeapp_generator import gen_fakeapp
-
-class PhaseFeatures:
-    def __init__(self, volume, mode, IOpattern, IOsize, nodes):
-        """Initializes the performance model with the phase features extracted from AppDecomposer
-        Args:
-            volume (int): volume of io will be generated
-            mode (string): read/write
-            IOpattern (string): Seq/Stride/Random
-            IOsize (string): size of IO
-            nodes (int): number of nodes, default = 1
-        """
-        self.volume = volume
-        self.mode = mode
-        self.IOpattern = IOpattern
-        self.IOsize = IOsize
-        self.nodes = nodes
-
-    def get_phase_srtring(self, sep=","):
-        s = str(self.volume) + sep + str(self.mode) + sep + str(self.IOpattern) + sep + str(self.IOsize) + sep + str(self.nodes)
-        return s
+from fakeapp_generator import fakeapp_generator
 
 class PhasePerformance:
 
@@ -44,19 +24,8 @@ class PhasePerformance:
         self.target = target
         self.ioi = ioi
         self.accelerator = accelerator
-        self.perf_data = pd.DataFrame()
 
-    def load_dataset(self, filename):
-        self.perf_data = pd.read_csv(filename)
-
-    def extract_phases(self):
-        # transform rows in the dataframe to a list of phase features
-        self.phases = self.perf_data.to_dict('records')
-
-    def export_to_csv(self, filename):
-        self.perf_data.to_csv(filename)
-
-    def get_perfomrances(self, sample=1):
+    def get_performances(self, sample=1):
         """Compute the performance on each tier with the phase features extracted from AppDecomposer
         Args:
             sample (int): number of sampling of the phase
@@ -76,11 +45,9 @@ class PhasePerformance:
 
         #update phase performance on the dataframe
         perf_df = pd.DataFrame()
-        perf_df["nfs"] = perf_nfs
-        perf_df["lfs"] = perf_lfs
-        perf_df["sbb"] = perf_sbb
-
-        self.perf_data.join(perf_df)
+        perf_df["nfs_bw"] = perf_nfs
+        perf_df["lfs_bw"] = perf_lfs
+        perf_df["sbb_bw"] = perf_sbb
 
         return perf_df
 
@@ -98,20 +65,67 @@ class PhasePerformance:
         #run fakeapp n times to get the avg bandwidth
         sum = 0
         for i in range(1, sample+1):
-            (t, bw) = gen_fakeapp(phase["volume"], phase["mode"], phase["IOpattern"],
+            (t, bw) = fakeapp_generator.gen_fakeapp(phase["volume"], phase["mode"], phase["IOpattern"],
                     phase["IOsize"], phase["nodes"], target, accelerator, self.ioi)
             sum += bw
         avg_bw = (float)(sum/sample)
         print("Performance on tier", target, accelerator, ": ", format(avg_bw/(1024*1024), '.2f'), "(Mb/s)")
         return avg_bw
 
+def load_dataset(filename):
+    data = pd.read_csv(filename)
+    return data
+
+def export_to_csv(data, filename):
+    data.to_csv(filename)
+
+class PerformanceModel:
+    def __init__(self, filename, targets,  accelerator="", ioi=False):
+        """Initializes the performance model with the phase features extracted from AppDecomposer
+        Args:
+            filename (string): file name of dataset in csv
+            targets (dict): storage backend file (nfs or lustre)
+            ioi (bool): enable/disable IOI, default = False
+            accelerator (string): using IO acclerator such as SBB/FIOL
+        """
+        self.filename = filename
+        self.targets = targets
+        self.accelerator = accelerator
+        self.ioi = ioi
+
+    def get_perfomance_table(self):
+
+        #load performance model from file
+        self.perf_data = load_dataset(self.filename)
+
+        print(self.perf_data)
+        if {'nfs_bw', 'lfs_bw', 'sbb_bw'}.issubset(self.perf_data.columns):
+            #update the performance table with new phases only
+            new_data = self.perf_data.loc[self.perf_data['nfs_bw'] == '']
+            phases = new_data.to_dict('records')
+
+        else:
+            # transform rows in the dataframe to a list of phase features
+            phases = self.perf_data.to_dict('records')
+            phases_perf = PhasePerformance(phases, self.targets, self.accelerator, self.ioi)
+            perf_df = phases_perf.get_performances()
+            self.perf_data = self.perf_data.join(perf_df)
+
+        return self.perf_data
+
 if __name__ == '__main__':
     target = dict(lfs="/fsiof/phamtt/tmp", nfs="/scratch/phamtt/tmp")
     acc = "SBB" # currently SBB works only with the lfs target
+    filename = "/home_nfs/phamtt/IO-SEA/iosea-wp3-recommandation-system/performance_model/dataset/performance_model_dataset.csv"
+    pm = PerformanceModel(filename, target, acc)
+    df = pm.get_perfomance_table()
+    print(df)
+"""
     phase0=dict(volume=100000000, mode="write", IOpattern="stride", IOsize=10000, nodes=1)
     phase1=dict(volume=100000000, mode="write", IOpattern="rand", IOsize=10000, nodes=1)
     phase2=dict(volume=100000000, mode="read", IOpattern="seq", IOsize=10000, nodes=1)
     phases = [phase0, phase1, phase2]
     perf_data = PhasePerformance(phases, target, acc)
-    df=perf_data.get_perfomrances(2)
+    df=perf_data.get_perfomrances(1)
     print(df)
+"""
