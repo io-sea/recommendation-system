30e1e371191c43f29c083bd6131ef9a28d2fcaf1
Salim Mimouni <salim.mimouni@atos.net>
2023-07-19 12:25:15 +0200

    feat: enhance resilience of data extraction and visualization functions
    - Update `extract_workflow_data` function to handle HTTP errors and return `None` when a request fails with a 500 error. This allows the function to continue extracting data from other workflows even if one fails.
    
    - Modify `display_timeseries` function to handle `None` values in the list of workflows, allowing it to skip over these and continue with the next item. This enhances the function's resilience when dealing with incomplete or missing data.
    
    - Refine the code in the workflow data extraction loop to handle the possibility of a `None` response from `extract_workflow_data`, and avoid filling the `central_jobs` dictionary with incomplete or missing data.
    
    These enhancements improve the robustness of the code, enabling it to handle unexpected situations and continue processing as much data as possible.

--- app_decomposer/app_decomposer/central_job.py
+++ app_decomposer/app_decomposer/central_job.py
@@ -11,7 +11,6 @@ from sklearn.decomposition import PCA
 import plotly.subplots as sp
 import math
 from sklearn.manifold import TSNE
-import umap
 
 
 
@@ -96,24 +95,33 @@ class WorkflowSearcher:
         df = pd.DataFrame(workflow_data)
         return df
 
+    
     def extract_workflow_data(self, workflow_id):
         # Set the endpoint and parameters for the request
         endpoint = f"/ioi/series/workflow/{workflow_id}"
         params = {"metrics_group": "volume"}
 
-        # Use the request_delegator method to make the GET request
-        response = self.connector.request_delegator("GET", endpoint, params=params)
+        try:
+            # Use the request_delegator method to make the GET request
+            response = self.connector.request_delegator("GET", endpoint, params=params)
+            
+            # Check for 500 error
+            response.raise_for_status()  # This will raise an HTTPError if the response was an error
 
-        data = response.json()
-        converted_data = {
-            workflow_id: {
-                "bytesRead": np.array([item["bytesRead"] for item in data]),
-                "bytesWritten": np.array([item["bytesWritten"] for item in data]),
-                "timestamp": np.array([item["timestamp"] for item in data]),
+            data = response.json()
+            converted_data = {
+                workflow_id: {
+                    "bytesRead": np.array([item["bytesRead"] for item in data]),
+                    "bytesWritten": np.array([item["bytesWritten"] for item in data]),
+                    "timestamp": np.array([item["timestamp"] for item in data]),
+                }
             }
-        }
-        
-        return converted_data
+
+            return converted_data        
+
+        except Exception as err:
+            print(f"An error occurred: {err}")
+            return None
 
 
 class CentralJob:
@@ -228,118 +236,285 @@ class CentralJob:
   
 
 
-  
-def display_features_3d(features, dim_reduction='pca', seed=42):
-    """
-    Display the features in 3D using PCA, t-SNE or UMAP and Plotly. The closest job to the centroid is highlighted.
-    """
-    features_wo_id = features.loc[:, features.columns != 'job_id']
-    job_ids = features['job_id'].index.values
 
-    # Perform dimensionality reduction on the features
-    if dim_reduction == 'pca':
-        reducer = PCA(n_components=3, random_state=seed)
-    elif dim_reduction == 'tsne':
-        reducer = TSNE(n_components=3, random_state=seed)
-    elif dim_reduction == 'umap':
-        reducer = umap.UMAP(n_components=3, random_state=seed)
+def display_features(features, central_job_id, dim_reduction='PCA', seed=42, 
+                     n_components=3, add_centroid=True):
+    """
+    Display the features using PCA, t-SNE and Plotly. The job closest to the centroid is highlighted.
+    """
+    
+    def add_centroid_to_features(df, job_id="centroid"):
+        """Adds a virtual job to the dataframe with features being the average of all other jobs."""
+        numeric_cols = df.select_dtypes(include=[np.number]).columns
+        centroid_features = df[numeric_cols].mean(axis=0)
+        centroid_df = pd.DataFrame([centroid_features], columns=df.columns)
+        centroid_df['job_id'] = job_id
+        return pd.concat([df, centroid_df])
+
+
+    # Reset index
+    features_df = features.reset_index()
+    if add_centroid:
+        features_df = add_centroid_to_features(features_df)
+    # Separate features from job_id
+    job_ids = features_df['job_id'].values
+    features = features_df.drop(columns='job_id')
+
+    # Perform dimensionality reduction
+    if dim_reduction == 'PCA':
+        reducer = PCA(n_components=n_components, random_state=seed)
+    elif dim_reduction == 't-SNE':
+        if features.shape[0] <= n_components:  # set perplexity value to be less than the number of samples
+            perplexity = features.shape[0] - 1
+        else:
+            perplexity = n_components
+        reducer = TSNE(n_components=n_components, random_state=seed, perplexity=perplexity)
     else:
-        raise ValueError(f"Invalid dim_reduction value: {dim_reduction}")
+        raise ValueError(f"Unknown dimensionality reduction method: {dim_reduction}")
 
-    reduced_features = reducer.fit_transform(features_wo_id)
+    reduced_features = reducer.fit_transform(features)
 
-    # Calculate the centroid
-    centroid = np.mean(reduced_features, axis=0)
+    if add_centroid:
+        # Create a dataframe from the reduced features
+        reduced_features_df = pd.DataFrame(reduced_features, 
+                                        index=job_ids, 
+                                        columns=[f"component_{i}" for i in range(1, n_components + 1)])
+        # Add a reduced_centroid to the dataframe
+        numeric_cols = reduced_features_df.columns
+        reduced_centroid_features = reduced_features_df.mean(axis=0)
+        reduced_centroid_df = pd.DataFrame(reduced_centroid_features).T  # Transpose to get it as a single row
+        reduced_centroid_df.index = ["reduced_centroid"]  # Set index value to 'reduced_centroid'
+        
+        # Append the reduced_centroid_df to the main dataframe
+        reduced_features_df = pd.concat([reduced_features_df, reduced_centroid_df])
+        # Update the reduced_features and job_ids variables
+        reduced_features = reduced_features_df.values
+        job_ids = reduced_features_df.index.values
 
-    # Find the index of the central job
-    distances = euclidean_distances(reduced_features, [centroid])
-    central_idx = np.argmin(distances)
+
+    # if add_centroid:
+    #     reduced_features_df = pd.DataFrame(reduced_features, 
+    #                                        index=job_ids, 
+    #                                        columns=[f"component_{i}" for i in range(1, n_components + 1)])
+    #     print(reduced_features_df)
+    #     reduced_features_df = add_centroid_to_features(reduced_features_df,
+    #                                                    job_id="reduced_centroid")
+    #     print(reduced_features_df)
+    #     reduced_features = reduced_features_df.to_numpy()
+    #     job_ids = reduced_features_df.index.values
+
+    # Calculate the variance explained by each principal component (if applicable)
+    explained_variance = np.sum(reducer.explained_variance_ratio_) if dim_reduction == 'PCA' else None
 
     # Create a Plotly figure
     fig = go.Figure()
 
     # Add each job's features to the figure as a scatter plot
-    for i, (feature, job_id) in enumerate(zip(reduced_features, job_ids)):
+    for i, (job_id, feature) in enumerate(zip(job_ids, reduced_features)):
         color = 'blue'
         size = 6
-        if i == central_idx:
-            # Highlight the central job
+        if job_id == central_job_id:  # Highlight the central job
             color = 'red'
             size = 8
-        fig.add_trace(go.Scatter3d(
-            x=[feature[0]],
-            y=[feature[1]],
-            z=[feature[2]],
-            mode='markers+text',
-            marker=dict(
-                size=size,
-                color=color,
-            ),
-            text=[f'Job {job_id}'],
-            name=f'Job {job_id}'
-        ))
+        elif job_id in ["centroid", "reduced_centroid"]:  # Highlight the centroid
+            color = 'gray'
+            size = 8
+        if n_components == 3:
+            fig.add_trace(go.Scatter3d(
+                x=[feature[0]],
+                y=[feature[1]],
+                z=[feature[2]],
+                mode='markers+text',
+                marker=dict(
+                    size=size,
+                    color=color,
+                ),
+                text=[str(job_id)[:5]],
+                name=str(job_id)[:5]
+            ))
+        elif n_components == 2:
+            fig.add_trace(go.Scatter(
+                x=[feature[0]],
+                y=[feature[1]],
+                mode='markers+text',
+                marker=dict(
+                    size=size,
+                    color=color,
+                ),
+                text=[str(job_id)[-9:]],
+                name=str(job_id)[-9:]
+            ))
+        else:
+            raise ValueError("Invalid number of components. n_components must be 2 or 3.")
 
-    # Add the centroid to the figure
-    fig.add_trace(go.Scatter3d(
-        x=[centroid[0]],
-        y=[centroid[1]],
-        z=[centroid[2]],
-        mode='markers',
-        marker=dict(
-            size=8,
-            color='black',
-            opacity=0.5
-        ),
-        name='Centroid'
-    ))
-
-    # Add the variance explained by each principal component to the figure's title
-    title = f"3D plot of features ({dim_reduction.upper()})"
-    if dim_reduction == 'pca':
-        explained_variance = np.sum(reducer.explained_variance_ratio_)
-        title += f" (explained variance: {explained_variance})"
-    fig.update_layout(title=title)
+    # Add the variance explained by each principal component to the figure's title (if applicable)
+    title = f"{n_components}D plot of features ({dim_reduction}~: {explained_variance})" if explained_variance else f"{n_components}D plot of features ({dim_reduction})"
+    fig.update_layout(title=title, height=600, width=800)
 
     # Return the figure
     return fig
 
 
 
-
-def display_timeseries(workflow_searcher, workflow_ids=None):
+def display_timeseries(workflows):
     """
-    Display the time series of bytesRead and bytesWritten for a list of workflow IDs using Plotly.
-    If no IDs are provided, all workflows will be plotted.
+    Display the time series of bytesRead and bytesWritten for a list of workflows using Plotly.
+    Args:
+        workflows (list of dict): A list of workflows. Each workflow is a dictionary 
+                                  with 'bytesRead', 'bytesWritten', and 'timestamp'.
     """
-    if workflow_ids is None:
-        workflow_ids = list(workflow_searcher.connector.workflows.keys())
-
-    n = len(workflow_ids)
-    n_cols = math.ceil(math.sqrt(n))
+    max_columns = 6
+    n = len(workflows)
+    n_cols = min(n, max_columns)
     n_rows = math.ceil(n / n_cols)
-    
-    fig = sp.make_subplots(rows=n_rows, cols=n_cols)
-    
-    for i, workflow_id in enumerate(workflow_ids):
+
+    fig = sp.make_subplots(rows=n_rows, cols=n_cols, subplot_titles=[list(w.keys())[0][-7:] if w is not None else 'None' for w in workflows])
+
+    for i, workflow_data in enumerate(workflows):
+        # Skip if workflow_data is None
+        if workflow_data is None:
+            continue
+
+        workflow_id = list(workflow_data.keys())[0]
+        job = workflow_data[workflow_id]
+
         row = i // n_cols + 1
         col = i % n_cols + 1
-
-        workflow_data = workflow_searcher.extract_workflow_data(workflow_id)
-        job = list(workflow_data.values())[0]
 
         fig.add_trace(go.Scatter(
             x=job['timestamp'],
             y=job['bytesRead'],
             mode='lines',
-            name=f'bytesRead_{workflow_id}'
+            name='bytesRead',
+            line_color='blue'
         ), row=row, col=col)
 
         fig.add_trace(go.Scatter(
             x=job['timestamp'],
             y=job['bytesWritten'],
             mode='lines',
-            name=f'bytesWritten_{workflow_id}'
+            name='bytesWritten',
+            line_color='orange'
         ), row=row, col=col)
 
-    fig.update_layout(height=400*n_rows, width=400*n_cols, title_text="Time series for Jobs")
+    fig.update_layout(height=200*n_rows, width=200*n_cols, title_text="Time series for Jobs", showlegend=False)
     return fig
+
+
+
+
+  
+# def display_features_3d(features, dim_reduction='pca', seed=42):
+#     """
+#     Display the features in 3D using PCA, t-SNE or UMAP and Plotly. The closest job to the centroid is highlighted.
+#     """
+#     features_wo_id = features.loc[:, features.columns != 'job_id']
+#     job_ids = features['job_id'].index.values
+
+#     # Perform dimensionality reduction on the features
+#     if dim_reduction == 'pca':
+#         reducer = PCA(n_components=3, random_state=seed)
+#     elif dim_reduction == 'tsne':
+#         reducer = TSNE(n_components=3, random_state=seed)
+#     elif dim_reduction == 'umap':
+#         reducer = umap.UMAP(n_components=3, random_state=seed)
+#     else:
+#         raise ValueError(f"Invalid dim_reduction value: {dim_reduction}")
+
+#     reduced_features = reducer.fit_transform(features_wo_id)
+
+#     # Calculate the centroid
+#     centroid = np.mean(reduced_features, axis=0)
+
+#     # Find the index of the central job
+#     distances = euclidean_distances(reduced_features, [centroid])
+#     central_idx = np.argmin(distances)
+
+#     # Create a Plotly figure
+#     fig = go.Figure()
+
+#     # Add each job's features to the figure as a scatter plot
+#     for i, (feature, job_id) in enumerate(zip(reduced_features, job_ids)):
+#         color = 'blue'
+#         size = 6
+#         if i == central_idx:
+#             # Highlight the central job
+#             color = 'red'
+#             size = 8
+#         fig.add_trace(go.Scatter3d(
+#             x=[feature[0]],
+#             y=[feature[1]],
+#             z=[feature[2]],
+#             mode='markers+text',
+#             marker=dict(
+#                 size=size,
+#                 color=color,
+#             ),
+#             text=[f'Job {job_id}'],
+#             name=f'Job {job_id}'
+#         ))
+
+#     # Add the centroid to the figure
+#     fig.add_trace(go.Scatter3d(
+#         x=[centroid[0]],
+#         y=[centroid[1]],
+#         z=[centroid[2]],
+#         mode='markers',
+#         marker=dict(
+#             size=8,
+#             color='black',
+#             opacity=0.5
+#         ),
+#         name='Centroid'
+#     ))
+
+#     # Add the variance explained by each principal component to the figure's title
+#     title = f"3D plot of features ({dim_reduction.upper()})"
+#     if dim_reduction == 'pca':
+#         explained_variance = np.sum(reducer.explained_variance_ratio_)
+#         title += f" (explained variance: {explained_variance})"
+#     fig.update_layout(title=title)
+
+#     # Return the figure
+#     return fig
+
+
+
+
+# def display_timeseries(workflow_searcher, workflow_ids=None):
+#     """
+#     Display the time series of bytesRead and bytesWritten for a list of workflow IDs using Plotly.
+#     If no IDs are provided, all workflows will be plotted.
+#     """
+#     if workflow_ids is None:
+#         workflow_ids = list(workflow_searcher.connector.workflows.keys())
+
+#     n = len(workflow_ids)
+#     n_cols = math.ceil(math.sqrt(n))
+#     n_rows = math.ceil(n / n_cols)
+    
+#     fig = sp.make_subplots(rows=n_rows, cols=n_cols)
+    
+#     for i, workflow_id in enumerate(workflow_ids):
+#         row = i // n_cols + 1
+#         col = i % n_cols + 1
+
+#         workflow_data = workflow_searcher.extract_workflow_data(workflow_id)
+#         job = list(workflow_data.values())[0]
+
+#         fig.add_trace(go.Scatter(
+#             x=job['timestamp'],
+#             y=job['bytesRead'],
+#             mode='lines',
+#             name=f'bytesRead_{workflow_id}'
+#         ), row=row, col=col)
+
+#         fig.add_trace(go.Scatter(
+#             x=job['timestamp'],
+#             y=job['bytesWritten'],
+#             mode='lines',
+#             name=f'bytesWritten_{workflow_id}'
+#         ), row=row, col=col)
+
+#     fig.update_layout(height=400*n_rows, width=400*n_cols, title_text="Time series for Jobs")
+#     return fig
